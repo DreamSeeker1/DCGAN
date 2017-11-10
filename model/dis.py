b813912ff94924837fb529cdd454502dd14db374
Yue <yuyue9284@outlook.com>
2017-11-10 10:24:46 +0800

    add bias initializer

--- model/dis.py
+++ model/dis.py
@@ -14,24 +14,29 @@ def discriminator(input_pics):
         # convolution layers
         conv1_layer = tf.layers.conv2d(input_pics, filters=128, activation=params.dis_conv_activation, kernel_size=5,
                                        strides=1,
+                                       bias_initializer=tf.truncated_normal_initializer,
                                        padding='same', name='conv1')
         pool1_layer = tf.layers.max_pooling2d(conv1_layer, pool_size=2, strides=2, name='pool1')
         conv2_layer = tf.layers.conv2d(pool1_layer, filters=256, activation=params.dis_conv_activation, kernel_size=5,
                                        strides=1,
+                                       bias_initializer=tf.truncated_normal_initializer,
                                        padding='same', name='conv2')
         pool2_layer = tf.layers.max_pooling2d(conv2_layer, pool_size=2, strides=2, name='pool2')
         conv3_layer = tf.layers.conv2d(pool2_layer, filters=512, activation=params.dis_conv_activation, kernel_size=5,
                                        strides=1,
+                                       bias_initializer=tf.truncated_normal_initializer,
                                        padding='same', name='conv3')
         pool3_layer = tf.layers.max_pooling2d(conv3_layer, pool_size=2, strides=2, name='pool3')
         conv4_layer = tf.layers.conv2d(pool3_layer, filters=1024, activation=params.dis_conv_activation, kernel_size=5,
                                        strides=1,
+                                       bias_initializer=tf.truncated_normal_initializer,
                                        padding='same', name='conv4')
         pool4_layer = tf.layers.max_pooling2d(conv4_layer, pool_size=2, strides=2, name='pool4')
 
         # reshape and project
         dense_input = tf.reshape(pool4_layer, shape=(-1, 4 * 4 * 1024))
-        dense1_layer = tf.layers.dense(dense_input, 100, activation=params.dis_dense_activation, name='dense1')
+        dense1_layer = tf.layers.dense(dense_input, 100, activation=params.dis_dense_activation,
+                                       bias_initializer=tf.truncated_normal_initializer, name='dense1')
         # prediction
         dense2_layer = tf.layers.dense(dense1_layer, 2, name='dense2')
     return dense2_layer
--- model/gen.py
+++ model/gen.py
@@ -13,16 +13,24 @@ def generator(input_batch):
     """
     with tf.variable_scope('Generator'):
         # project and reshape
-        layer1 = tf.layers.dense(input_batch, units=4 * 4 * 1024)
+        layer1 = tf.layers.dense(input_batch, bias_initializer=tf.truncated_normal_initializer, units=4 * 4 * 1024)
         reshape_input = tf.reshape(layer1, shape=(-1, 4, 4, 1024))
 
         # convolution transpose
         conv1_trans = tf.layers.conv2d_transpose(reshape_input, filters=512, kernel_size=5, strides=(2, 2),
-                                                 padding='same', activation=params.gen_activation, name='conv1_trans')
+                                                 padding='same',
+                                                 bias_initializer=tf.truncated_normal_initializer,
+                                                 activation=params.gen_activation, name='conv1_trans')
         conv2_trans = tf.layers.conv2d_transpose(conv1_trans, filters=256, kernel_size=5, strides=(2, 2),
-                                                 padding='same', activation=params.gen_activation, name='conv2_trans')
+                                                 padding='same',
+                                                 bias_initializer=tf.truncated_normal_initializer,
+                                                 activation=params.gen_activation, name='conv2_trans')
         conv3_trans = tf.layers.conv2d_transpose(conv2_trans, filters=128, kernel_size=5, strides=(2, 2),
-                                                 padding='same', activation=params.gen_activation, name='conv3_trans')
+                                                 padding='same',
+                                                 bias_initializer=tf.truncated_normal_initializer,
+                                                 activation=params.gen_activation, name='conv3_trans')
         pics_batch = tf.layers.conv2d_transpose(conv3_trans, filters=3, kernel_size=5, strides=(2, 2),
-                                                padding='same', activation=params.gen_activation, name='conv4_trans')
+                                                padding='same',
+                                                bias_initializer=tf.truncated_normal_initializer,
+                                                activation=params.gen_activation, name='conv4_trans')
         return pics_batch
