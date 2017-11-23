fix batch normalization

model/dis.py
@@ -1,6 +1,11 @@
 """Discriminator used to discriminate real pics from generated pics"""
 import tensorflow as tf
 import model.params as params
+from model.params import bn as bn
+
+# activation function for convolution layer and dense layer
+dis_act = params.dis_activation
+dens_act = params.dis_dense_activation
 
 
 def discriminator(input_pics, reuse, drop_prob):
@@ -13,29 +18,36 @@ def discriminator(input_pics, reuse, drop_prob):
         logits: the logits of the model
     """
     with tf.variable_scope('Discriminator', reuse=reuse):
-        # use the batch norm
-        input_pics = tf.contrib.layers.batch_norm(input_pics)
-        # convolution layers
-        conv1_layer = tf.layers.conv2d(input_pics, filters=64, activation=params.dis_conv_activation, kernel_size=5,
+        # define the batch normalization layers
+        bn0 = bn(name='bn0')
+        bn1 = bn(name='bn1')
+        bn2 = bn(name='bn2')
+        bn3 = bn(name='bn3')
+
+        # convolution layers and batch norm layers
+        conv1_layer = tf.layers.conv2d(input_pics, filters=64, kernel_size=5,
                                        strides=2,
                                        bias_initializer=params.dis_bias_initializer,
                                        padding='same', name='conv1')
-        conv2_layer = tf.layers.conv2d(conv1_layer, filters=128, activation=params.dis_conv_activation, kernel_size=5,
+        conv1_layer = dis_act(bn0(conv1_layer))
+        conv2_layer = tf.layers.conv2d(conv1_layer, filters=128, kernel_size=5,
                                        strides=2,
                                        bias_initializer=params.dis_bias_initializer,
                                        padding='same', name='conv2')
-        conv3_layer = tf.layers.conv2d(conv2_layer, filters=256, activation=params.dis_conv_activation, kernel_size=5,
+        conv2_layer = dis_act(bn1(conv2_layer))
+        conv3_layer = tf.layers.conv2d(conv2_layer, filters=256, kernel_size=5,
                                        strides=2,
                                        bias_initializer=params.dis_bias_initializer,
                                        padding='same', name='conv3')
-        conv4_layer = tf.layers.conv2d(conv3_layer, filters=512, activation=params.dis_conv_activation, kernel_size=5,
+        conv3_layer = dis_act(bn2(conv3_layer))
+        conv4_layer = tf.layers.conv2d(conv3_layer, filters=512, kernel_size=5,
                                        strides=2,
                                        bias_initializer=params.dis_bias_initializer,
                                        padding='same', name='conv4')
-
+        conv4_layer = dis_act(bn3(conv4_layer))
         # reshape and project
         dense_input = tf.reshape(conv4_layer, shape=(-1, 4 * 4 * 512))
         dense_input_drop = tf.nn.dropout(dense_input, keep_prob=1 - drop_prob)
-        output_layer = tf.layers.dense(dense_input_drop, 1, activation=params.dis_dense_activation,
+        output_layer = tf.layers.dense(dense_input_drop, 1,
                                        bias_initializer=params.dis_bias_initializer, name='output')
     return output_layer

model/gen.py
@@ -2,6 +2,7 @@
 
 import tensorflow as tf
 import model.params as params
+from model.params import bn
 
 
 def generator(input_batch, drop_prob):
@@ -12,33 +13,48 @@ def generator(input_batch, drop_prob):
     Returns:
         pics_batch: the picture generator generates
     """
+    # activation function for convolution layer and dense layer
+    gen_act = params.gen_activation
+    output_act = params.gen_output_activation
+
     with tf.variable_scope('Generator'):
-        # add batch normalization
-        input_batch = tf.contrib.layers.batch_norm(input_batch)
+        # define batch normalization layers
+        bn0 = bn(name='bn0')
+        bn1 = bn(name='bn1')
+        bn2 = bn(name='bn2')
+        bn3 = bn(name='bn3')
+
         # project and reshape
         layer1 = tf.layers.dense(input_batch, units=4 * 4 * 1024)
         reshape_input = tf.reshape(layer1, shape=(-1, 4, 4, 1024))
+        # use batch normalization
+        reshape_input = gen_act(bn0(reshape_input))
 
         # convolution transpose
         conv1_trans = tf.layers.conv2d_transpose(reshape_input, filters=512, kernel_size=5, strides=(2, 2),
                                                  padding='same',
                                                  bias_initializer=params.gen_bias_initializer,
-                                                 activation=params.gen_activation, name='conv1_trans')
+                                                 name='conv1_trans')
+        # use batch normalization
+        conv1_trans = gen_act(bn1(conv1_trans))
+        # use dropout
         conv1_trans_drop = tf.nn.dropout(conv1_trans, keep_prob=1 - drop_prob)
         conv2_trans = tf.layers.conv2d_transpose(conv1_trans_drop, filters=256, kernel_size=5, strides=(2, 2),
                                                  padding='same',
                                                  bias_initializer=params.gen_bias_initializer,
-                                                 activation=params.gen_activation, name='conv2_trans')
+                                                 name='conv2_trans')
+        conv2_trans = gen_act(bn2(conv2_trans))
         conv2_trans_drop = tf.nn.dropout(conv2_trans, keep_prob=1 - drop_prob)
         conv3_trans = tf.layers.conv2d_transpose(conv2_trans_drop, filters=128, kernel_size=5, strides=(2, 2),
                                                  padding='same',
                                                  bias_initializer=params.gen_bias_initializer,
-                                                 activation=params.gen_activation, name='conv3_trans')
+                                                 name='conv3_trans')
+        conv3_trans = gen_act(bn3(conv3_trans))
         conv3_trans_drop = tf.nn.dropout(conv3_trans, keep_prob=1 - drop_prob)
         # use Tanh in output layer
         pics_batch = tf.layers.conv2d_transpose(conv3_trans_drop, filters=params.channel, kernel_size=5, strides=(2, 2),
                                                 padding='same',
                                                 bias_initializer=params.gen_bias_initializer,
-                                                activation=params.gen_activation, name='conv4_trans')
-        pics_batch = params.gen_output_activation(pics_batch)
+                                                activation=gen_act, name='conv4_trans')
+        pics_batch = output_act(pics_batch)
         return pics_batch

model/params.py
@@ -5,9 +5,12 @@ gen_activation = tf.nn.relu
 gen_output_activation = tf.nn.tanh
 gen_bias_initializer = tf.zeros_initializer
 # activation function used in discriminator
-dis_conv_activation = tf.nn.leaky_relu
+dis_activation = tf.nn.leaky_relu
 dis_dense_activation = tf.nn.leaky_relu
 dis_bias_initializer = tf.zeros_initializer
 
 # the number of channels
 channel = 3
+
+# batch normalization layer for D and G
+bn = tf.layers.BatchNormalization

run.py
@@ -33,13 +33,13 @@ def get_labels(gen_pics, real_pics):
 graph = tf.Graph()
 with graph.as_default():
     # load data
-    dataset = data_utils.tf_utils.get_pics('./pics').batch(params.batch_size).shuffle(2 * params.batch_size)
+    dataset = data_utils.tf_utils.get_pics('./pics').batch(params.batch_size)
     iterator = dataset.make_initializable_iterator()
     next_element = iterator.get_next()
 
     # define the place holder for the input.
-    gen_input = tf.placeholder(dtype=tf.float32, shape=(None, 100))
-    pics_input = tf.placeholder(dtype=tf.float32, shape=(None, 64, 64, mparams.channel))
+    gen_input = tf.placeholder(dtype=tf.float32, shape=(None, 100), name="noise")
+    pics_input = tf.placeholder(dtype=tf.float32, shape=(None, 64, 64, mparams.channel), name="real_pics")
     drop_gen = tf.placeholder(dtype=tf.float32, shape=(), name='drop_prob_gen')
     drop_dis = tf.placeholder(dtype=tf.float32, shape=(), name='drop_prob_dis')
     # generate pictures